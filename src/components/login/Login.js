ba833cd022f4f7e62bd8842ce62af1a452a3bab4
yasinabdmahmood <abd181103@gmail.com>
2023-03-12 21:06:13 +0300

    Add login logic

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-function Login(props) {
-    return (
-        <div>
-            <h1>Log in page</h1>
-        </div>
-    );
-}
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-export default Login;
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3000/employees/sign_in', {
+        employee: {
+          email: email,
+          password: password
+        }
+      },{
+        withCredentials: true
+      });
+      console.log(response.data);
+      navigate('/home');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
