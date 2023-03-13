8173b9aa4453e46dc24c908c1d8402c7d28e7735
yasinabdmahmood <abd181103@gmail.com>
2023-03-13 16:30:00 +0300

    Add redux template code

---
+++ src/redux/login/loginReducer.js
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import baseUrl from '../baseUrl';
+import axios from 'axios';
+
+const url = baseUrl + '/employees/sign_in';
+
+export const getLoggedInCredentials = createAsyncThunk(
+  'getLoggedInCredentials/',
+  async (payloadData) => {
+    console.log('payloadData')
+    console.log(payloadData)
+    try {
+      return axios.post(url, {
+        employee: {
+          email: payloadData.email,
+          password: payloadData.password
+        }
+      },{
+        withCredentials: true
+      });
+    } catch (error) {
+      return error;
+    }
+  },
+);
+
+const initialState = null;
+
+export const LoginSlice = createSlice({
+  name: 'loggedInEmployee',
+  initialState,
+  reducers: {
+    clearSession() {
+      return null;
+    },
+
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getLoggedInCredentials.fulfilled, (state, action) => {
+      const loggedInEmployee = action.payload.data;
+      return loggedInEmployee;
+    });
+  },
+});
+export const { clearSession } = LoginSlice.actions;
+export default LoginSlice.reducer;
