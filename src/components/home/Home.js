b0f2bc8058f7a6dd411badd51a5c8795c77ac07c
yasinabdmahmood <abd181103@gmail.com>
2023-03-17 00:37:09 +0300

    Delete unneccessary components

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,12 +4,6 @@ import Template from '../template/Template';
 import Login from '../login/Login';
 import Signup from '../signup/Signup';
 import Dashboard from '../dashboard/Dashboard';
-import ManageInVehicle from '../manage_in_vehicle/ManageInVehicle'
-import ManageOutVehicle from '../manage_out_vehicle/ManageOutVehicle'
-import AddCategory from '../add_category/AddCategory'
-import ManageCategory from '../manage_category/ManageCategory'
-import BetweenDatesReport from '../between_dates_report/BetweenDatesReport'
-import SearchVehicle from '../search_vehicle/SearchVehicle';
 import RegUser from '../reg_user/RegUser';
 import Employees from '../employees/Employees';
 import SubscriptionTypes from '../sunbscription_types/SubscriptionTypes';
@@ -33,12 +27,6 @@ function Home() {
             <Route path="clients" element={<Clients/>} />
             <Route path="subscriptionRecords" element={<SubscriptionRecords/>} />
             <Route path="paymentRecords" element={<PaymentRecords/>} />
-            <Route path="manage_in_vehicle" element={<ManageInVehicle/>} />
-            <Route path="manage_out_vehicle" element={<ManageOutVehicle/>} />
-            <Route path="add_category" element={<AddCategory/>} />
-            <Route path="manage_category" element={<ManageCategory/>} />
-            <Route path="search_vehicle" element={<SearchVehicle/>} />
-            <Route path="between_dates_report" element={<BetweenDatesReport/>} />
             <Route path="reg_user" element={<RegUser/>} />
           </Route>
         </Routes>
