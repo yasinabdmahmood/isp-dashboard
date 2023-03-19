5f06c77013ad252516f14053859a3612ef299c46
yasinabdmahmood <abd181103@gmail.com>
2023-03-19 12:56:10 +0300

    Add NewSubscriptionRecord to handle subscription record creation

--- src/components/new_subscription_record/NewSubscriptionRecord.js
+++ src/components/new_subscription_record/NewSubscriptionRecord.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { createSubscriptionRecord, getPaymentRecords } from '../../redux/database/databaseReducer';
+
+
 
 function NewSubscriptionRecord() {
+    const subscriptionTypes = useSelector( state => state.database.subscriptionTypes);
+    const clients = useSelector( state => state.database.clients);
+    const [client, setClient] = useState('')
+    const [pay, setPay] = useState('');
+    const [subscriptionType, setSubscriptionType] = useState(subscriptionTypes[0].id)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      const payloadData = {
+          clientId: clients.find( el => el.name === client).id,
+          employeeId: JSON.parse(sessionStorage.getItem('user')).id ,
+          subscriptionTypeId: subscriptionType,
+          pay: pay,
+      }
+      dispatch(createSubscriptionRecord(payloadData));
+      dispatch(getPaymentRecords());
+      navigate('/home/subscriptionRecords')
+    };
+  
+  
     return (
-        <div>
-            <h1>New subscription record</h1>
-        </div>
+      <Form onSubmit={handleSubmit} className='d-flex flex-column align-items-center' >
+        <FormGroup className='d-flex flex-column'>
+            <Label for="client">Client</Label>
+            <input type="text" name="client" className='bg-white' id="cleint" placeholder="Type to search" value={client} onChange={(e) => setClient(e.target.value)} list="clients" />
+            <datalist id="clients">
+                {clients.filter(el => el.name.toLowerCase().includes(client.toLowerCase())).map(el => (
+                <option key={el.id} value={el.name}  />
+                ))}
+            </datalist>
+        </FormGroup>
+        
+        <FormGroup className='d-flex flex-column'>
+            <Label for="subscription-type">Subscription Type</Label>
+                <select name="subscription-type" className='bg-white' id="subscription-type" value={subscriptionType} onChange={(e) => setSubscriptionType(e.target.value)}>
+                    {subscriptionTypes.map( subscriptionType => {
+                        return <option value={subscriptionType.id}>{subscriptionType.category}</option>
+                    })}
+                </select>
+        </FormGroup>
+
+        <FormGroup>
+          <Label for="paid-amount">Paid amount</Label>
+          <Input type="number" name="paid-amount" className='bg-white' id="pay" required value={pay} onChange={(e) => setPay(e.target.value)} />
+        </FormGroup>
+        <Button color="primary" type="submit">Create Subscription Type</Button>
+      </Form>
     );
 }
 
