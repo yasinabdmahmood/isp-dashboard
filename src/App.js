3b36b09e00a688df21a323ad16ed576fadbd9a65
yasinabdmahmood <abd181103@gmail.com>
2023-03-21 18:18:01 +0300

    Bring data into redux store in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.scss'
 import Home from './components/home/Home'
+import { getEmployees, getClients, getPaymentRecords, getSubscriptionRecords, getSubscriptionTypes} from './redux/database/databaseReducer'
 function App() {
+  const dispatch = useDispatch();
+  useEffect(()=> {
+    dispatch(getEmployees());
+    dispatch(getClients());
+    dispatch(getPaymentRecords());
+    dispatch(getSubscriptionRecords());
+    dispatch(getSubscriptionTypes());
+  },[dispatch])
   return (
     <div className="App">
       <Home />
