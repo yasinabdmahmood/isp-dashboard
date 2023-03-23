45c995d219379516be74a5453b8adda4d9d98e25
yasinabdmahmood <abd181103@gmail.com>
2023-03-23 15:25:18 +0300

    Implement ShowClient component

--- src/components/show_client/ShowClient.js
+++ src/components/show_client/ShowClient.js
@@ -1,11 +1,114 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteClient, deleteSubscriptionRecord, getPaymentRecords } from '../../redux/database/databaseReducer';
+import { useNavigate } from 'react-router';
 import { useParams } from 'react-router';
 
 function ShowClient() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
     const {id} = useParams();
+    const client = useSelector(state => state.database?.clients?.find( cl => cl.id === parseInt(id)));
+    const subscriptionRecords = useSelector(state => state.database?.subscriptionRecords?.filter( el => el.client_id === parseInt(id)));
+    const handleClientDeletion = (id) => {
+        const confirm = window.confirm('Are you sure you want to delete this client')
+        if(confirm){
+            dispatch(deleteClient(id));
+        }
+        else{
+            return;
+        }
+        
+    };
+
+    const handleSubscriptionRecordDeletion = async (id) => {
+        // Display alert message and prompt user to continue or cancel
+        const confirmed = window.confirm('Are you sure you want to delete this item?');
+        
+        if (confirmed) {
+          const payload = {id};
+          await dispatch(deleteSubscriptionRecord(payload));
+          dispatch(getPaymentRecords());
+        } else {
+          // User clicked Cancel, do nothing
+          return;
+        }
+      }
     return (
         <div>
-           <h1>Show client {id}</h1> 
+           <h1>Show client {id}</h1>
+           <div className="container d-flex justify-content-center mt-5">
+             <table className="table table-striped w-75">
+               <thead className="thead-dark" >
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">User name</th>
+                        <th scope="col">Contact Info</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        <tr key={client?.id}>
+                        <td>{client?.name}</td>
+                        <td>{client?.username}</td>
+                        <td>{client?.client_contact_informations[0]?.contact_info || 'No contact information'}</td>
+                        <td>
+                            <button
+                            className='btn btn-sm btn-danger m-1'
+                            onClick={()=>{handleClientDeletion(client?.id)}}
+                            >
+                             Delete
+                            </button>
+                            <button
+                            className='btn btn-sm btn-secondary m-1'
+                            onClick={()=>{navigate(`/home/clients/edit/${client?.id}`)}}
+                            >
+                             Edit
+                            </button>
+                        </td>
+                        </tr>       
+                </tbody>
+      </table>
+    </div> 
+    <div className="container d-flex justify-content-center mt-5">
+             <table className="table table-striped w-75">
+                <thead className="thead-dark" >
+                <tr>
+                    <th scope="col">Client</th>
+                    <th scope="col">Employee</th>
+                    <th scope="col">Subscription Type</th>
+                    <th scope="col">Paid Amount</th>
+                    <th scope="col">Remaining Amount</th>
+                    <th scope="col">Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                {subscriptionRecords?.map(subscriptionRecord => (
+                    <tr key={subscriptionRecord.id}>
+                    <td>{subscriptionRecord.client.name}</td>
+                    <td>{subscriptionRecord.employee.name}</td>
+                    <td>{subscriptionRecord.subscription_type.category}</td>
+                    <td>{subscriptionRecord.pay}</td>
+                    <td>{subscriptionRecord.subscription_type.cost - subscriptionRecord.pay}</td>
+                    <td>
+                        <button 
+                        className='btn btn-sm btn-danger m-1'
+                        onClick={() => handleSubscriptionRecordDeletion(subscriptionRecord.id)}
+                        >
+                            Delete
+                        </button>
+                        <button 
+                        className='btn btn-sm btn-secondary m-1'
+                        onClick={()=>navigate(`/home/paymentRecords/new/${subscriptionRecord.id}`)}
+                        >
+                            Add payment
+                        </button>
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+              </table>
+            </div>
         </div>
     );
 }
