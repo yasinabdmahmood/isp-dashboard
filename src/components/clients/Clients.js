e5f78e59bdad1d1dcf21dce4cdeea49a5c90dea6
yasinabdmahmood <abd181103@gmail.com>
2023-03-23 13:56:36 +0300

    Add search feature to clients list

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,12 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { getClients, deleteClient } from '../../redux/database/databaseReducer';
 
 function Clients() {
+    const [search, setSearch] = useState('');
+    const [searchType, setSearchType] = useState('name');
     const navigate = useNavigate();
     const clients = useSelector( state => state.database.clients);
     const dispatch = useDispatch();
+    const filterClients = (client) => {
+        if (searchType === 'name' && client.name.toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+        if (searchType === 'username' && client.username.toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+        if (searchType === 'phone' && client.client_contact_informations[0]?.contact_info?.toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+        return false;
+      };
     const handleClientDeletion = (id) => {
         const confirm = window.confirm('Are you sure you want to delete this client')
         if(confirm){
@@ -26,6 +40,21 @@ function Clients() {
     },[]);
     return (
         <div>
+           <div className='d-flex flex-column justify-content-center align-items-center m-3'>
+            <div className='m-1'>
+            <span>Search by</span>
+                <select value={searchType} onChange={(e) => setSearchType(e.target.value)}>
+                    <option value="name">Name</option>
+                    <option value="username">Username</option>
+                    <option value="phone">Phone Number</option>
+                </select>
+            </div>
+                <div className='m-1'>
+                    <span>Search</span>
+                <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+                </div>
+                
+           </div>
            <div className="container d-flex justify-content-center mt-5">
              <table className="table table-striped w-75">
                <thead className="thead-dark" >
@@ -37,7 +66,7 @@ function Clients() {
                     </tr>
                 </thead>
                 <tbody>
-                    {clients?.map(client => (
+                    {clients?.filter(client => filterClients(client)).map(client => (
                         <tr key={client.id}>
                         <td>{client.name}</td>
                         <td>{client.username}</td>
