1fbf00289076a67d1bcc02f5fdf85f0dcdcc78a6
yasinabdmahmood <abd181103@gmail.com>
2023-03-27 02:24:06 +0300

    Add delete button to payment history list

--- src/components/payment_history/PaymentHistory.js
+++ src/components/payment_history/PaymentHistory.js
@@ -3,12 +3,26 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import formatDate from '../../helpers/formatDate';
-import { clearPaymentHistory, getPaymentHistory } from '../../redux/database/databaseReducer';
+import { clearPaymentHistory, getPaymentHistory, deletePaymentRecord, filterPaymentHistory } from '../../redux/database/databaseReducer';
 
 function PaymentHistory() {
     const {id} = useParams()
     const dispatch = useDispatch();
-    const paymentRecords = useSelector(state=>state.database?.paymentHistory)
+    const paymentRecords = useSelector(state=>state.database?.paymentHistory);
+
+    const handleDeletion = async (id) => {
+      // Display alert message and prompt user to continue or cancel
+      const confirmed = window.confirm('Are you sure you want to delete this item?');
+      
+      if (confirmed) {
+        const payload = {id};
+        await dispatch(deletePaymentRecord(payload));
+        dispatch(filterPaymentHistory({id}))
+      } else {
+        // User clicked Cancel, do nothing
+        return;
+      }
+    }
 
     useEffect(()=>{
         const fetchData = async() => {
@@ -34,6 +48,7 @@ function PaymentHistory() {
                     <th scope="col">Employee</th>
                     <th scope="col">Amount</th>
                     <th scope="col">Date</th>
+                    <th scope="col">Action</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -43,6 +58,13 @@ function PaymentHistory() {
                     <td>{paymentRecord.employee.name}</td>
                     <td>{paymentRecord.amount}</td>
                     <td>{formatDate(paymentRecord.created_at)}</td>
+                    <td>
+                      <button
+                      className='btn btn-sm btn-danger'
+                      onClick={()=>handleDeletion(paymentRecord.id)}>
+                        Delete
+                      </button>
+                    </td>
                     </tr>
                 ))}
                 </tbody>
