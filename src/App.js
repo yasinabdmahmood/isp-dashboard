a0c4226769c1cf74a480b80fc0132fe65371c0d3
yasinabdmahmood <abd181103@gmail.com>
2023-03-28 17:15:52 +0300

    Make request to get cookies before sign in

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import axios from 'axios'
 import './App.scss'
 import Home from './components/home/Home'
-import { getEmployees, getClients, getSubscriptionTypes} from './redux/database/databaseReducer'
+
 function App() {
-  const dispatch = useDispatch();
   useEffect(()=> {
-    console.log('MyComponent rendered');
-    const fetchData = async() => {
-    // await dispatch(getEmployees());
-    // await dispatch(getClients());
-    // await dispatch(getSubscriptionTypes());
-    }
-    fetchData()
-  },[dispatch])
+    axios.get('https://isp-system.onrender.com/employees/sign_in'
+      ,{
+        withCredentials: true
+      }).then((response) => {
+      console.log('Got Cookie')
+    });
+    
+  },[])
   return (
     <div className="App">
       <Home />
