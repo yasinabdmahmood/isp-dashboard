08b6e32380f475a31af8e858a66a01fb032b3c7a
yasinabdmahmood <abd181103@gmail.com>
2023-03-29 01:14:56 +0300

    Ex5

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import './App.scss'
 import Home from './components/home/Home'
-import baseUrl from './redux/baseUrl'
+import { useDispatch } from 'react-redux';
+import { loggout } from './redux/login/loginReducer';
+
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
   useEffect(()=> {
-        axios.get('https://isp-system.onrender.com/employees/sign_in',{
-          withCredentials: true
-        }).then(()=>{
-          setLoading(false)
-        })
+        // axios.get('https://isp-system.onrender.com/employees/sign_in',{
+        //   withCredentials: true
+        // }).then(()=>{
+        //   setLoading(false)
+        // })
+        dispatch(loggout());
   },[])
   if(loading){
     return <div>Loading...</div>
