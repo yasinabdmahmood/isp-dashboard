c25e72de97bf4b4eae79139d27546a9071a0e614
yasinabdmahmood <abd181103@gmail.com>
2023-03-29 15:58:49 +0300

    Chnage the base url for development mode

--- src/redux/baseUrl.js
+++ src/redux/baseUrl.js
@@ -1,2 +1,2 @@
-const baseUrl = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'? 'http://127.0.0.1:3000':'https://isp-system.onrender.com';
+const baseUrl = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'? 'http://localhost:3000':'https://isp-system.onrender.com';
 export default baseUrl;
