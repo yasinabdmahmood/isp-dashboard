dfdfab07d9f237554050b5e062673c66ec5c8dbf
yasinabdmahmood <abd181103@gmail.com>
2023-03-31 15:26:44 +0300

    Fix the bug of the case if user types in invalid client in subscription record creation form

--- src/components/new_subscription_record/NewSubscriptionRecord.js
+++ src/components/new_subscription_record/NewSubscriptionRecord.js
@@ -39,15 +39,19 @@ function NewSubscriptionRecord() {
   
     const handleSubmit = async(event) => {
       event.preventDefault();
+      const clientId = clients.find( el => el.name === client)?.id
+      if(!clientId){
+        window.alert('This user does not exist')
+        return;
+      }
       const payloadData = {
-          clientId: clients.find( el => el.name === client).id,
+          clientId: clientId,
           employeeId: JSON.parse(sessionStorage.getItem('user')).id ,
           subscriptionTypeId: subscriptionType,
           pay: pay,
       }
       const response = await dispatch(createSubscriptionRecord(payloadData));
       if(response.type.includes('fulfilled')){
-        await dispatch(getPaymentRecords());
         navigate('/home/subscriptionRecords')
       }else{
         window.alert('The action failed, please try again')
@@ -67,7 +71,7 @@ function NewSubscriptionRecord() {
       <Form onSubmit={handleSubmit} className='d-flex flex-column align-items-center' >
         <FormGroup className='d-flex flex-column'>
             <Label for="client">Client</Label>
-            <input type="text" name="client" className='bg-white' id="cleint" placeholder="Type to search" value={client} onChange={(e) => setClient(e.target.value)} list="clients" />
+            <input type="text" name="client" className='bg-white' id="cleint" placeholder="Type to search" value={client} onChange={(e) => setClient(e.target.value)} list="clients" autocomplete="off"/>
             <datalist id="clients">
                 {clients.filter(el => el.name.toLowerCase().includes(client.toLowerCase())).map(el => (
                 <option key={el.id} value={el.name}  />
