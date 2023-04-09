1ed38bdc23fee597e8e0cc6b1a65d887db35be52
yasinabdmahmood <abd181103@gmail.com>
2023-04-09 17:49:56 +0300

    Add date and time field to payment record creation form

--- src/components/new_payment_record/NewPaymentRecord.js
+++ src/components/new_payment_record/NewPaymentRecord.js
@@ -3,10 +3,16 @@ import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import { useDispatch } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { createPaymentRecord } from '../../redux/database/databaseReducer';
+import { convertToRailsDateTime } from '../../helpers/formatDate';
+import DateTimePicker from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
+
 
 function NewPaymentRecord() {
   const {id} = useParams();
   const [amount, setAmount] = useState('');
+  const [dateTime, setDateTime] = useState('');
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -15,6 +21,7 @@ function NewPaymentRecord() {
     const payloadData = {
         amount: amount,
         subscription_record_id: id,
+        created_at: convertToRailsDateTime(dateTime),
     }
     const response = await dispatch(createPaymentRecord(payloadData));
     if(response.type.includes('fulfilled')){
@@ -35,6 +42,23 @@ function NewPaymentRecord() {
             <Label for="cost">Amount</Label>
             <Input type="number" name="cost" className='bg-white' id="cost" required value={amount} onChange={(e) => setAmount(e.target.value)} />
         </FormGroup>
+        <FormGroup>
+          <Label for="datetime">Date and Time</Label>
+          <DateTimePicker
+            id="datetime"
+            value={dateTime}
+            onChange={(value) => setDateTime(value)}
+            inputProps={{
+              className: `bg-white`,
+              autoComplete: 'off',
+              placeholder: 'Enter date and time',
+            }}
+            dateFormat="YYYY/MM/DD"
+            timeFormat="h:mm A"
+            closeOnSelect
+          />
+        </FormGroup>
+
         <Button color="primary" className='btn-sm' type="submit">Create</Button>
         </Form>
     </div>
