2a35aff1be03d2c068fba64e9a8d4f624ae13f41
yasinabdmahmood <abd181103@gmail.com>
2023-04-09 17:36:29 +0300

    Add another helper function to convert date and time format from react format to rails format

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,16 +1,35 @@
-function formatDate(dateTimeStr) {
+export const formatDate = (dateTimeStr) => {
+    function padNumber(num) {
+      return num.toString().padStart(2, '0');
+    }
     const date = new Date(dateTimeStr);
     const year = date.getFullYear();
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const hours = date.getHours() % 12 || 12;
     const minutes = date.getMinutes();
-    const ampm = date.getHours() >= 12 ? 'pm' : 'am';
+    const ampm = date.getHours() >= 12 ? 'AM' : 'PM';
     return `${year}/${padNumber(month)}/${padNumber(day)}  ${hours}:${padNumber(minutes)} ${ampm}`;
   }
   
-  function padNumber(num) {
-    return num.toString().padStart(2, '0');
-  }
+  
 
-  export default formatDate;
+  export const convertToRailsDateTime = (dateTimeStr) => {
+    function padNumber(num) {
+      return num.toString().padStart(2, '0');
+    }
+    const date = new Date(dateTimeStr);
+    const year = date.getFullYear();
+    const month = padNumber(date.getMonth() + 1);
+    const day = padNumber(date.getDate());
+    const hours = padNumber(date.getHours() % 12 || 12);
+    const minutes = padNumber(date.getMinutes());
+    const seconds = padNumber(date.getSeconds());
+    const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${ampm}`;
+  }
+  
+  
+  
+
+  
