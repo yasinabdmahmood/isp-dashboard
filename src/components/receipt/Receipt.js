a9d56974d2bbab4968f17ebd48745eee8af0193d
yasinabdmahmood <abd181103@gmail.com>
2023-04-26 00:09:10 +0300

    Change the template of the receipe with its styling

--- src/components/receipt/Receipt.js
+++ src/components/receipt/Receipt.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import styles from './Receipt.module.css';
 import { useNavigate } from 'react-router-dom';
+import { formatDate } from '../../helpers/formatDate';
 
 
 const Receipt = () => {
@@ -16,27 +17,31 @@ const Receipt = () => {
     <div className={styles.container}>
       <div className={styles.header}>
         <h2>Receipt</h2>
-        <p>Date: 22/04/2023</p>
+        <p>{formatDate(new Date())}</p>
       </div>
       <div className={styles.items}>
         <div className={styles.item}>
-          <span className={styles.itemName}>Item 1</span>
+          <span className={styles.itemName}>:اسم المشترك</span>
           <span className={styles.itemPrice}>$10.00</span>
         </div>
         <div className={styles.item}>
-          <span className={styles.itemName}>Item 2</span>
+          <span className={styles.itemName}>:تاريخ والوقت الاشتراك</span>
           <span className={styles.itemPrice}>$20.00</span>
         </div>
         <div className={styles.item}>
-          <span className={styles.itemName}>Item 3</span>
+          <span className={styles.itemName}>:تاريخ الدفع</span>
           <span className={styles.itemPrice}>$15.00</span>
         </div>
         <div className={styles.item}>
-          <span className={styles.itemName}>Item 4</span>
+          <span className={styles.itemName}>:الموضف</span>
           <span className={styles.itemPrice}>$8.50</span>
         </div>
         <div className={styles.item}>
-          <span className={styles.itemName}>Item 5</span>
+          <span className={styles.itemName}>:المبلغ</span>
+          <span className={styles.itemPrice}>$12.25</span>
+        </div>
+        <div className={styles.item}>
+          <span className={styles.itemName}>:مجموع</span>
           <span className={styles.itemPrice}>$12.25</span>
         </div>
       </div>
