b0dffde8edea659124111dfcfd774ac6993bb88d
yasinabdmahmood <abd181103@gmail.com>
2023-04-30 16:37:29 +0300

    Make the template for receipt

--- src/components/receipt/Receipt.js
+++ src/components/receipt/Receipt.js
@@ -1,54 +1,106 @@
 import React, { useEffect } from 'react';
 import styles from './Receipt.module.css';
-import { useNavigate } from 'react-router-dom';
-import { formatDate } from '../../helpers/formatDate';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { convertToRailsDateTime, formatDate } from '../../helpers/formatDate';
+import internetDish from '../../assets/images/dish.png'
 
 
 const Receipt = () => {
+  const location = useLocation();
+  const subscriptionRecord = location.state?.subscriptionRecord;
 
   const navigate = useNavigate()  
+
+  const getUserName = () => {
+    let loggedInEmployee = sessionStorage.getItem('user');
+    let retrievedData = JSON.parse(loggedInEmployee);
+    return retrievedData.name
+  }
   
   useEffect(()=>{
     window.print();
     navigate(-1);
   })
 
+  // return (
+  //   <div className={styles.container}>
+  //     <div className={styles.header}>
+  //       <h2>Receipt</h2>
+  //       <p>{formatDate(new Date())}</p>
+  //     </div>
+  //     <div className={styles.items}>
+  //       <div className={styles.item}>
+  //         <span className={styles.itemName}>:اسم المشترك</span>
+  //         <span className={styles.itemPrice}>{subscriptionRecord.client.name}</span>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <span className={styles.itemName}>:تاريخ والوقت الاشتراك</span>
+  //         <span className={styles.itemPrice}>{convertToRailsDateTime(subscriptionRecord.created_at)}</span>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <span className={styles.itemName}>:تاريخ الدفع</span>
+  //         <span className={styles.itemPrice}>{formatDate(new Date())}</span>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <span className={styles.itemName}>:الموضف</span>
+  //         <span className={styles.itemPrice}>{getUserName()}</span>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <span className={styles.itemName}>:المبلغ</span>
+  //         <span className={styles.itemPrice}>$12.25</span>
+  //       </div>
+  //       <div className={styles.item}>
+  //         <span className={styles.itemName}>:مجموع</span>
+  //         <span className={styles.itemPrice}>{subscriptionRecord.pay} IQD</span>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.header}>
-        <h2>Receipt</h2>
-        <p>{formatDate(new Date())}</p>
-      </div>
-      <div className={styles.items}>
-        <div className={styles.item}>
-          <span className={styles.itemName}>:اسم المشترك</span>
-          <span className={styles.itemPrice}>$10.00</span>
+        <h1>بابان نيت لخدمات الانترنيت</h1>
+        <div>
+          <h2>0770 359 88 00</h2>
+          <h2>0770 161 85 71</h2>
         </div>
-        <div className={styles.item}>
-          <span className={styles.itemName}>:تاريخ والوقت الاشتراك</span>
-          <span className={styles.itemPrice}>$20.00</span>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.itemName}>:تاريخ الدفع</span>
-          <span className={styles.itemPrice}>$15.00</span>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.itemName}>:الموضف</span>
-          <span className={styles.itemPrice}>$8.50</span>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.itemName}>:المبلغ</span>
-          <span className={styles.itemPrice}>$12.25</span>
-        </div>
-        <div className={styles.item}>
-          <span className={styles.itemName}>:مجموع</span>
-          <span className={styles.itemPrice}>$12.25</span>
+        <div>
+          <img src={internetDish} alt="Dish" />
         </div>
       </div>
-      <div className={styles.footer}>
-        <span className={styles.total}>Total: $66.75</span>
+      <div className={styles.date}>
+        <span>202 / / :تاريخ دفع الاشتراك</span>
       </div>
-    </div>
+      <div className={styles['fields-container']}>
+        <div className={styles['field-container']}>
+          <div className={styles.lable}>:استلمت من السيد</div>
+          <div className={styles.value}>Name</div>
+        </div>
+        <div className={styles['field-container']}>
+          <div className={styles.lable}>:مبلغ و قدره</div>
+          <div className={styles.value}>Name</div>
+        </div>
+        <div className={styles['field-container']}>
+          <div className={styles.lable}>:الملاحضة</div>
+          <div className={styles.value}>Name</div>
+        </div>
+        <div className={styles['field-container']}>
+          <div className={styles.lable}>:رقم الموبايل</div>
+          <div className={styles.value}>Name</div>
+        </div>
+        <div className={styles['field-container']}>
+          <div className={styles.lable}>:تاريخ تفعيل الاشتراك</div>
+          <div className={styles.value}>Name</div>
+        </div>
+      </div>
+      <div className={styles.address}>
+        <span>شورجة - قرب جسر شورجة - مقابل غسل كامران</span>
+      </div>
+      <div className={styles.signiture}>
+        <span>توقيع المستلم</span>
+      </div>
+    </>
   );
 };
 
