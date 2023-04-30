362505a5b66687fd9f54b91b92d90d2c785bb7ba
yasinabdmahmood <abd181103@gmail.com>
2023-04-30 19:40:00 +0300

    Modify receipt component

--- src/components/receipt/Receipt.js
+++ src/components/receipt/Receipt.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import styles from './Receipt.module.css';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { convertToRailsDateTime, formatDate } from '../../helpers/formatDate';
-import internetDish from '../../assets/images/dish.png'
+import internetDish from '../../assets/images/powerbeam.png'
 
 
 const Receipt = () => {
@@ -70,28 +70,32 @@ const Receipt = () => {
         </div>
       </div>
       <div className={styles.date}>
-        <span>202 / / :تاريخ دفع الاشتراك</span>
+        <span>{convertToRailsDateTime(subscriptionRecord.created_at)}:تاريخ دفع الاشتراك</span>
       </div>
       <div className={styles['fields-container']}>
         <div className={styles['field-container']}>
+          <div className={styles.lable}>:اسم الموضف</div>
+          <div className={styles.value}>{getUserName()}</div>
+        </div>
+        <div className={styles['field-container']}>
           <div className={styles.lable}>:استلمت من السيد</div>
-          <div className={styles.value}>Name</div>
+          <div className={styles.value}>{subscriptionRecord.client.name}</div>
         </div>
         <div className={styles['field-container']}>
           <div className={styles.lable}>:مبلغ و قدره</div>
-          <div className={styles.value}>Name</div>
+          <div className={styles.value}>{subscriptionRecord.pay} IQD</div>
         </div>
         <div className={styles['field-container']}>
           <div className={styles.lable}>:الملاحضة</div>
-          <div className={styles.value}>Name</div>
+          <div className={styles.value}></div>
         </div>
         <div className={styles['field-container']}>
           <div className={styles.lable}>:رقم الموبايل</div>
-          <div className={styles.value}>Name</div>
+          <div className={styles.value}></div>
         </div>
         <div className={styles['field-container']}>
           <div className={styles.lable}>:تاريخ تفعيل الاشتراك</div>
-          <div className={styles.value}>Name</div>
+          <div className={styles.value}>{convertToRailsDateTime(subscriptionRecord.created_at)}</div>
         </div>
       </div>
       <div className={styles.address}>
