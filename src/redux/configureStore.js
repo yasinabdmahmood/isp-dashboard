254873e77259ed03a728d26afb82d81a1647f7a6
yasinabdmahmood <abd181103@gmail.com>
2023-05-21 22:32:32 +0300

    Add app state to configurationStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,10 +2,12 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import loginReducer from './login/loginReducer';
 import databaseReducer from './database/databaseReducer';
+import AppStateReducer from './app-state/appState';
 
 const rootReducer = combineReducers({
   database: databaseReducer,
   login: loginReducer,
+  appState: AppStateReducer,
 
 });
 const store = configureStore({
