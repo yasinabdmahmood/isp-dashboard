1d85f78ba7fe8db6ae08581c8f3a19620441c981
yasinabdmahmood <abd181103@gmail.com>
2023-05-25 09:57:20 +0300

    Integrate the new loading technique into

--- src/components/payment_records/PaymentRecords.js
+++ src/components/payment_records/PaymentRecords.js
@@ -10,18 +10,19 @@ function PaymentRecords() {
     const paymentRecords = useSelector(state => state.database.paymentRecords);
     const dispatch = useDispatch();
     const elementRef = useRef(null);
-    const [loading, setLoading] = useState(true);
+    
     const [loadMoreButton, setLoadMoreButton] = useState(true);
+
     useEffect(()=>{
         async function fetchData() {
             if(paymentRecords.length < 20){
                const response = await dispatch(getPaymentRecords());
-               if(response.type.includes('fulfilled')){
-                setLoading(false);
+               if(!response.type.includes('fulfilled')){
+                alert('Some thing went wrong, please try again !!')
                }
             }
             else{
-              setLoading(false)
+              
             }
         }
         fetchData();
@@ -35,10 +36,10 @@ function PaymentRecords() {
         const payload = {id};
         const response = await dispatch(deletePaymentRecord(payload));
         if(response.type.includes('fulfilled')){
-          window.alert('Item deleted successfully')
+          window.alert('Item deleted successfully');
         }
         else{
-          window.alert('Failed to delete the item')
+          window.alert('Failed to delete the item');
         }
       } else {
         // User clicked Cancel, do nothing
@@ -52,30 +53,6 @@ function PaymentRecords() {
         setLoadMoreButton(false);
       }
     }
-
-    // function  handleScroll() {
-    //     const element = elementRef.current;
-  
-    //     if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
-    //       dispatch(getPaymentRecords());
-    //     }
-    //   }
-  
-    //   useEffect(() => {
-        
-    
-    //     const element = elementRef.current;
-    //     element.addEventListener('scroll', handleScroll);
-    
-    //     return () => {
-    //       element.removeEventListener('scroll', handleScroll);
-    //     };
-    //   }, [handleScroll]);
-  
-      // if (loading) {
-      //   return <div ref={elementRef}>Loading...</div>;
-      // }
-
 
     return (
         <div ref={elementRef} style={{ height: '900px', overflow: 'auto' }}>
