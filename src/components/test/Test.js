3277fdfca7e8ba48d55a5c7d1d441f4baf76931a
yasinabdmahmood <abd181103@gmail.com>
2023-10-20 20:45:26 +0300

    Modify Test component to display the test data in table

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -2,14 +2,19 @@ import React from 'react';
 import styles from './styles.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
 import { getTestData } from '../../redux/database/databaseReducer';
-
 import Reducer from '../../redux/helpers/reducer';
+import { Table } from 'react-bootstrap';
 const {getAgents} = Reducer.asyncThunks
 
 function Test() {
 
-    const testData = useSelector( state => state.database?.agents?.data);
+    const testData = useSelector( state => state.database?.test);
     const dispatch = useDispatch();
+    const data = [
+        { id: 1, name: 'John', age: 30 },
+        { id: 2, name: 'Alice', age: 25 },
+        { id: 3, name: 'Bob', age: 35 },
+      ];
 
     const fetchCompanies = () => {
         const payload = {requested_data: 'company'};
@@ -18,7 +23,7 @@ function Test() {
 
     const fetchAgents = () => {
         const payload = {requested_data: 'agent'};
-        dispatch(getAgents())
+        dispatch(getTestData(payload))
     }
 
     const fetchLedgers = () => {
@@ -28,12 +33,57 @@ function Test() {
     return (
         <div>
             <h1>This is test page</h1>
-            <h2>{testData || "No data has been fetched yes"}</h2>
             <button onClick={fetchAgents}>Fetch agents</button>
             <button onClick={fetchCompanies}>Fetch companies</button>
             <button onClick={fetchLedgers}>Fetch ledgers</button> 
+
+            <div className='mt-5'>
+                
+               <MyTable data={ testData} />
+            </div>
         </div>
     );
 }
+
+
+function MyTable({ data }) {
+ 
+    
+  if (!data || data.length === 0) {
+    return <p>No data available</p>;
+  }
+
+  // Get the keys (table headers) from the first object in the array
+  const headers = Object.keys(data[0]);
+  console.log('0000000000000000')
+    console.log(headers)
+    console.log('0000000000000000')
+
+  return (
+    <Table striped bordered hover responsive>
+      <thead>
+        <tr>
+          {headers.map((header, index) => (
+            <th key={index}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, rowIndex) => (
+          <tr key={rowIndex}>
+            {headers.map((header, colIndex) => (
+              <td key={colIndex}>
+                {item[header]}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      </Table>
+  );
+}
+
+
+
 
 export default Test;
