4e420dc8a9b8b017de782ba62e759486ad852c6d
yasinabdmahmood <abd181103@gmail.com>
2023-10-28 09:09:33 +0300

    Add new action for updatnig agent

--- src/redux/database/agentReducers.js
+++ src/redux/database/agentReducers.js
@@ -37,7 +37,26 @@ export default [
           agents: state.agents.filter( (agent) => agent.id !== parseInt(agent_id))
        };
     }
-  }
+  },
+
+  {
+   name: 'updateAgent',
+   url: '/agents/update_agent',
+   method: 'get',
+   reducer: (state, action) => {
+      const updatedAgent = action.payload.data;
+      return {
+         ...state,
+         agents: state.agents.map( (agent) => {
+            if(updatedAgent.id === parseInt(agent.id)){
+               return updatedAgent;
+            }else{
+               return agent;
+            }
+         })
+      };
+   }
+ }
   
 ].map( (item) =>  new Reducer(item) )
 
