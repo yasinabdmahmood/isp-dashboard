9bfe749d613c0af298421918edd6c644c87171fa
yasinabdmahmood <abd181103@gmail.com>
2023-11-12 22:58:33 +0300

    Create two components for displaying and creating ledger

---
+++ src/components/ledger_components/create_ledger/CreateLedger.js
@@ -0,0 +1,165 @@
+import React, { useEffect, useState, useReducer } from 'react';
+import styles from './styles.module.scss';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import DatePicker from 'react-datetime';
+
+import Reducer from '../../../redux/helpers/reducer';
+const {createLedger, getCompanies, getAgents} = Reducer.asyncThunks
+
+function parseDate(dateTimeStr) {
+  function padNumber(num) {
+    return num.toString().padStart(2, '0');
+  }
+  const date = new Date(dateTimeStr);
+  const year = date.getFullYear();
+  const month = padNumber(date.getMonth() + 1);
+  const day = padNumber(date.getDate());
+
+  return`${year}-${month}-${day}`;
+}
+
+function slectedCompaniesReducer(state, action) {
+  let newState = null
+  switch (action.type) {
+    case 'setValue':
+      newState =  { ...state, [action.key]: action.value };
+      break;
+    case 'removeKey': 
+      newState = {...state};
+      newState = Object.keys(state).reduce((acc, key) => {
+        if (key !== action.key) {
+          acc[key] = newState[key];
+        }
+        return acc;
+      }, {});
+      break;
+    case 'addKey':
+      if(state.hasOwnProperty(action.key)){
+        newState = {...state};
+      }else{
+        newState = {...state, [action.key]: 0}
+      }
+      break;
+    default:
+      newState =  {...state};
+  }
+  console.log(newState);
+  return newState;
+}
+
+function CreateLedger() {
+    const [agentId, setAgentId] = useState(null);
+    const [selectedCompanies, customDispatch] = useReducer(slectedCompaniesReducer, {});
+    const [selectedCompany, setSelectedCompany] = useState(null);
+    const [date, setDate] = useState(new Date());
+    
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const companies = useSelector( state => state.database.companies);
+    const agents = useSelector( state => state.database.agents);
+
+    useEffect(()=>{
+      if(companies.length === 0){
+        dispatch(getCompanies())
+      }
+      if(agents.length === 0){
+        dispatch(getAgents())
+      }
+    },[dispatch])
+
+    useEffect(() => {
+      if(selectedCompany === null && companies.length > 0){
+        setSelectedCompany(companies[0]?.id)
+      }
+      
+    },[companies]);
+
+    useEffect(() => {
+      if(agentId === null && agents.length > 0){
+        setAgentId(agents[0]?.id)
+      }
+      
+    },[companies]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    
+    const payloadData = {
+       agent_id: parseInt(agentId),
+       detail: selectedCompanies,
+       date: parseDate(date),
+    }
+    console.log(payloadData)
+    const response = await dispatch(createLedger(payloadData))
+    if(response.type.includes('fulfilled')){
+      window.alert('The item was created successfully')
+      navigate('/home/ledgers')
+    }else{
+      window.alert('The action failed, please try again')
+    }
+  };
+ 
+  return (
+    <>
+    <h2 className='text-start h4 m-4'>Create new Ledger</h2>
+    <Form onSubmit={handleSubmit} className='d-flex flex-column align-items-center' >
+    <FormGroup className='d-flex flex-column'>
+            <Label for="subscription-type">Agent</Label>
+                <select name="subscription-type" className={`bg-white`} id="subscription-type" value={agentId} onChange={(e) => setAgentId(e.target.value)}>
+                    {agents.map( agent => {
+                        return <option value={agent.id}>{agent.name}</option>
+                    })}
+                </select>
+    </FormGroup>
+
+    <FormGroup>
+      <Label for="date">Date</Label>
+      <DatePicker
+            id="date"
+            value={date}
+            onChange={(value) => setDate(value)}
+            inputProps={{
+
+              
+              className: `bg-white ${styles.inputfield}`,
+              autoComplete: 'off',
+              placeholder: 'Enter date',
+            }}
+            dateFormat="YYYY/MM/DD"
+            closeOnSelect
+          />
+    </FormGroup>
+
+      {Object.keys(selectedCompanies).map((company)=>{
+        return (
+          <FormGroup>
+            <Label for={company}>{company}</Label>
+            <Input type="number" name={company} className='bg-white' id="profit"  value={selectedCompanies[company]} onChange={(e) => customDispatch({type:"setValue",value:parseInt(e.target.value),key:company})} />
+            <Input type="button" onClick={(e) => {e.preventDefault();customDispatch({ type: "removeKey", key: company })}} name={company} className='bg-danger' id="profit"  value={"Delete"} />
+          </FormGroup>
+        )
+      })}
+
+      <Button color="primary" className='btn-sm' type="submit">Create</Button>
+    </Form>
+    <Form  onSubmit={(e) => {e.preventDefault();customDispatch({type: "addKey", key: e.target.companyType.value})}} className='d-flex flex-column align-items-center' >
+      <FormGroup className='d-flex flex-column'>
+              <Label for="subscription-type">Add type</Label>
+                  <select name="companyType" className={`bg-white`} id="subscription-type" value={selectedCompany} onChange={(e) => setSelectedCompany(e.target.value)}>
+                      {companies.map( company => {
+                          return <option value={company.name}>{company.name}</option>
+                      })}
+                  </select>
+      </FormGroup>
+      <Button color="primary"  className='btn-sm' type="submit">Add new type</Button>
+    </Form>
+    </>
+  );
+}
+
+export default CreateLedger;
---
+++ src/components/ledger_components/ledgers/Ledgers.js
@@ -0,0 +1,111 @@
+import React, {useEffect, useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getClients, deleteClient } from '../../../redux/database/databaseReducer';
+import isAdmin from '../../../helpers/isAdmin';
+import Table from 'react-bootstrap/Table';
+import styles from './styles.module.scss';
+import plusSign from '../../../assets/images/plus-circle.svg';
+import edit from '../../../assets/images/pencil-square.svg';
+import trash from '../../../assets/images/trash-fill.svg';
+import view from '../../../assets/images/eye-fill.svg';
+import searchLogo from '../../../assets/images/search.svg';
+import Reducer from '../../../redux/helpers/reducer';
+const {getLedgers} = Reducer.asyncThunks
+
+function Ledgers() {
+    const [search, setSearch] = useState('');
+    const navigate = useNavigate();
+    const clients = useSelector( state => state.database.clients);
+    const ledgers = useSelector( state => state.database.ledgers);
+    const dispatch = useDispatch();
+    const filterAgents = (ledger) => {
+        console.log(ledger)
+        const isAgentName = ledger?.agent?.name.toLowerCase().includes(search.toLowerCase());
+        if(isAgentName){
+            return true
+        }
+        return false;
+      };
+    const handleClientDeletion = async(id) => {
+        const confirm = window.confirm('Are you sure you want to delete this client')
+        if(confirm){
+            const response = await dispatch(deleteClient(id));
+            if(response.type.includes('fulfilled')){
+                window.alert('Item was deleted successfully');
+            }
+            else{
+                window.alert('Failed to delete the item, please try again')
+            }
+        }
+        else{
+            return;
+        }
+        
+    }
+    useEffect(() => {
+        // if the employees list has not been fetched from server before
+        // then fetch the list
+        if(clients.length === 0){
+            dispatch(getClients())
+        }
+        if(ledgers.length === 0){
+            dispatch(getLedgers())
+        }
+    },[dispatch]);
+    return (
+        <div className={styles.container}>
+            <div className='d-flex flex-column flex-sm-row justify-content-between align-items-center'>
+                <h3 className='text-start text-black m-4 h4'>Ledgers</h3>
+                <div className='d-flex  justify-content-center align-items-stretch mx-5'>
+                        <div className={styles['search-container']}>
+                            <img src={searchLogo} className={styles['search-icon']} style={{background: 'white'}} alt='search' />
+                            <input className={styles['search-input']} placeholder='search' type="text" value={search} onChange={(e) => setSearch(e.target.value)} />        
+                        </div> 
+                </div>
+            </div>
+           
+           <div className="p-sm-3 mt-2">
+           <Table striped bordered hover responsive>
+               <thead className="thead-dark" >
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Withdraw</th>
+                        <th scope="col">Deposit</th>
+                        <th scope="col">Date</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {ledgers?.filter(ledger => filterAgents(ledger)).map(ledger => (
+                        <tr key={ledger.id}>
+                        <td>{ledger.agent.name}</td>
+                        <td>{ledger.withdraw}</td>
+                        <td>{ledger.deposit}</td>
+                        <td>{ledger.date}</td>
+                        <td className='d-flex justify-content-around align-items-center'>
+                        { isAdmin() &&
+                            <>
+                                    <img src={trash} style={{cursor: 'pointer'}} onClick={()=>{handleClientDeletion(ledger.id)}} className='mx-2'/>
+                                    <img src={edit} style={{cursor: 'pointer'}} onClick={()=>{navigate(`/home/clients/edit/${ledger.id}`)}} className='mx-2'/>
+                            </>
+                        }
+                          <img src={view} style={{cursor: 'pointer'}} onClick={()=>{navigate(`/home/clients/${ledger.id}`)}} className='mx-2'/>
+                           
+                        </td>
+                        </tr>
+                    ))}
+                </tbody>
+      </Table>
+    </div>
+    { isAdmin() && <div className={styles['plus-sign']}>
+            <button onClick={()=> navigate('/home/ledgers/new')} >
+                <img src={plusSign} alt='Add sign' />
+            </button>
+        </div>
+    }
+    </div>
+    );
+}
+
+export default Ledgers ;
