4c6da4a11e8293a5b87e7c754fd8448b86b9582d
yasinabdmahmood <abd181103@gmail.com>
2023-11-18 23:07:53 +0300

    Add date to the ledger details page

--- src/components/ledger_components/ledger_detail/LedgerDetail.js
+++ src/components/ledger_components/ledger_detail/LedgerDetail.js
@@ -16,46 +16,46 @@ function LedgerDetail(props) {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        if(!ledger){
+        if (!ledger) {
             dispatch(getLedgers());
         }
-    },[]);
+    }, []);
     return (
         <div className={styles.container}>
             <div className='d-flex flex-column flex-sm-row justify-content-between align-items-center'>
                 <h3 className='text-start text-black m-4 h4'>Ledger Detail</h3>
             </div>
             <div className='m-3'>
-            <Table striped bordered hover responsive>
+                <Table striped bordered hover responsive>
                     <thead className="thead-dark" >
-                            <tr>
-                                <th>Name</th>
-                                <td>{ledger.agent.name}</td>
-                            </tr>
-                            <tr>
-                                <th>Info</th>
-                                <td>{ledger.agent.info}</td>
-                            </tr>
+                        <tr>
+                            <th>Name</th>
+                            <td>{ledger.agent.name}</td>
+                        </tr>
+                        <tr>
+                            <th>Info</th>
+                            <td>{ledger.agent.info}</td>
+                        </tr>
                     </thead>
                 </Table>
                 <Table striped bordered hover responsive>
                     <thead className="thead-dark" >
-                            <tr>
-                                <th scope="col">Type</th>
-                                <th scope="col">Amount</th>
-                            </tr>
+                        <tr>
+                            <th scope="col">Type</th>
+                            <th scope="col">Amount</th>
+                        </tr>
                     </thead>
                     <tbody>
-                            {  
-                               Object.keys(ledger? ledger.detail: {})?.map((type, index) => (
-                                    <tr key={index}>
-                                        <td>{type}</td>
-                                        <td>{ledger.detail[type]}</td>
-                                    </tr>
-                               
-                               ))
-                            }
-                               
+                        {
+                            Object.keys(ledger ? ledger.detail : {})?.map((type, index) => (
+                                <tr key={index}>
+                                    <td>{type}</td>
+                                    <td>{ledger.detail[type]}</td>
+                                </tr>
+
+                            ))
+                        }
+
 
                     </tbody>
                     <tfoot>
@@ -67,8 +67,12 @@ function LedgerDetail(props) {
                             <th scope="col">Deposit amount</th>
                             <th scope="col">{ledger?.deposit}</th>
                         </tr>
+                        <tr>
+                            <th scope="col">Date</th>
+                            <th scope="col">{ledger?.date}</th>
+                        </tr>
                     </tfoot>
-               </Table>
+                </Table>
             </div>
         </div>
     );
